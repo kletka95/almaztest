2b9eadf043b9fec2841ac4ca5cd365ea2c6d7a1a
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-26 21:14:12 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -1,61 +1,14 @@
 package main
 
-import (
-    "fmt"
-)
-
-func add(a, b float64) float64 {
-    return a + b
-}
-
-func subtract(a, b float64) float64 {
-    return a - b
-}
-
-func multiply(a, b float64) float64 {
-    return a * b
-}
-
-func divide(a, b float64) float64 {
-    if b == 0 {
-        fmt.Println("Ошибка: деление на ноль!")
-        return 0
-    }
-    return a / b
-}
-
-func getInput() (float64, float64, string) {
-    var a, b float64
-    var operator string
-
-    fmt.Print("Введите первое число: ")
-    fmt.Scanln(&a)
-
-    fmt.Print("Введите второе число: ")
-    fmt.Scanln(&b)
+import "fmt"
 
 
-    return a, b, operator
-}
 
 func main() {
-    a, b, operator := getInput()
+    var sec int
 
-    var result float64
+    fmt.Scan(&first, &sec)
+    fmt.Println()
+    fmt.Println(first+sec)
 
-    switch operator {
-    case "+":
-        result = add(a, b)
-    case "-":
-        result = subtract(a, b)
-    case "*":
-        result = multiply(a, b)
-    case "/":
-        result = divide(a, b)
-    default:
-        fmt.Println("Неизвестный оператор")
-        return
-    }
-
-    fmt.Printf("Результат: %.2f\n", result)
 }
