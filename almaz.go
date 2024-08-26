37030f90d2a9320cb766f3a477e7a0642040b10b
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-26 21:08:16 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -1,9 +1,44 @@
-
 package main
 
 import (
     "fmt"
 )
+
+func add(a, b float64) float64 {
+    return a + b
+}
+
+func subtract(a, b float64) float64 {
+    return a - b
+}
+
+func multiply(a, b float64) float64 {
+    return a * b
+}
+
+func divide(a, b float64) float64 {
+    if b == 0 {
+        fmt.Println("Ошибка: деление на ноль!")
+        return 0
+    }
+    return a / b
+}
+
+func getInput() (float64, float64, string) {
+    var a, b float64
+    var operator string
+
+    fmt.Print("Введите первое число: ")
+    fmt.Scanln(&a)
+
+    fmt.Print("Введите второе число: ")
+    fmt.Scanln(&b)
+
+    fmt.Print("Введите оператор (+, -, *, /): ")
+    fmt.Scanln(&operator)
+
+    return a, b, operator
+}
 
 func main() {
     a, b, operator := getInput()
