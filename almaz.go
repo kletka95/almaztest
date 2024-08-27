3464ebe0b4f114f619d492f0e3700c7221324cea
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-27 11:06:55 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -9,26 +9,7 @@ import (
 
 
 func main() {
-    romeToArab := map[string]int {
-                'I': 1,
-                'V': 5,
-                'X': 10,
-                'L': 50,
-                'C': 100,
-                'D': 500,
-                'M': 1000,
-            
-    }
-    arabToRome := map[int]string{
 
-            1: 'I',
-            5: 'V',
-            10: 'X',
-            50: 'L',
-            100: 'C',
-    }
-   res := romeToArab["I"] + romeToArab["II"]
-   fmt.Println(romeToArab[res])
     var first int
     var sec int
     var oper string
