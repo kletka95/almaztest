f19856338e3ddb0a676c3c1ae442ceac6ebccc20
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-27 10:25:20 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -12,14 +12,14 @@ func main() {
     var first int
     var sec int
     var oper string
+    var c bool
     fmt.Scan(&first, &oper, &sec)
-        switch first{
-             case  &first > 10:
-                fmt.Println("Не больше 10")
-                }
-        switch sec{
-            case  &sec > 10:
-                fmt.Println("Не больше 10")
+        switch c{
+            case first > 10:
+                fmt.Println("ture")
+            case sec > 10:
+                fmt.Println("true too")
+
                 }
         switch oper{
             case "+":
