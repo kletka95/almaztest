84cc36be2c7f8ee5af23cd7280209c7960159e9d
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-27 15:36:52 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -29,10 +29,5 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	
 	fmt.Println(a...:"Введите что-то")
-	text, _ :=  reader.ReadString(delim: '\n')
-	text = strings.TrimSpace(text)
-	toNumber, _ := strconv.Atoi(text)
-	fmt.Println(toNumber+8)
-	
 	
 }
