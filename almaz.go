e7692ad6b8a7219d1417badfd184400e1ad8c2ef
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-27 15:38:34 +0300

    Update almaz.go

--- almaz.go
+++ almaz.go
@@ -6,14 +6,30 @@ import (
 	"strconv"
 	"strings"
 )
+var romanMap = []struct {
+	decVal int
+	symbol string
+}{
+    {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+	{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
+}
+func decimalToRomanRecursive(num int) string {
+	if num == 0 {
+    	return ""
+	}
+	for _, pair := range romanMap {
+    	if num >= pair.decVal {
+            return pair.symbol + decimalToRomanRecursive(num-pair.decVal)
+        }
+    }
+	return ""
+}
+
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	
 	fmt.Println(a...:"Введите что-то")
-	text, _ :=  reader.ReadString(delim: '\n')
-	text = strings.TrimSpace(text)
-	toNumber, _ := strconv.Atoi(text)
-	fmt.Println(toNumber+8)		
 	
 }
