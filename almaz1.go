8130962fc8d08aa4c4f6c23e021e1f0ac591f736
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-27 17:12:41 +0300

    Update almaz1.go

--- almaz1.go
+++ almaz1.go
@@ -128,7 +128,7 @@ var i int
  fmt.Println("Ввести")
  text, _ := reader.ReadString('\n')
  
- fmt.Sscanf(text, "%5d", &i)
+ fmt.Sscanf(text, "%20d", &i)
  input := text
  result, err := calculate(input)
  if err != nil {
