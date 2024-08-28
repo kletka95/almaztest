c97d0370d5e3eea23296a249d58b8dcfba4c4e08
kletka95 <110083876+kletka95@users.noreply.github.com>
2024-08-28 13:55:53 +0300

    Update aa.go

--- aa.go
+++ aa.go
@@ -21,16 +21,16 @@ var romanArabic = map[string]int {
 }
 
 var arabicRoman = map[int]string {
-  "I",
-   "II",
-  "III",
-   "IV",
-  "V",
-   "VI",
-   "VII",
-  "VIII",
-   "IX",
-  "X",
+  1: "I",
+  2:  "II",
+  3: "III",
+  4:  "IV",
+  5: "V",
+  6:  "VI",
+  7:  "VII",
+  8: "VIII",
+  9:  "IX",
+  10: "X",
   }
 
 
